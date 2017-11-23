d9f5eb6ee5bfb32176fd19a7867cf9f00d0be748
Regani <bohdan.konechniy@gmail.com>
2017-11-24 00:10:06 +0200

    Lab 8

--- web_lab_5/js/my.js
+++ web_lab_5/js/my.js
@@ -1,61 +1,77 @@
-$(function() {
-    $('#send_review_button').click(function(){
+var idNewsMask = 'newId_';
 
-    	var userName = document.getElementById('send_user_name').value;
-    	var date = new Date();
-			var dateString = "";
-			dateString = date.getDate() + "." + date.getMonth() + "." + (date.getFullYear())
-				+ ", " + date.getHours() + ":" + date.getMinutes();
-		var reviewText = document.getElementById('send_review').value;
+//Show all existing reviews 
+window.onload = function() {
+	$.fn.showNews();
+};
 
-    	var newDiv = '<div class="review divider_bottom"><div class="user_name"><h1>' 
-    	+ userName 
-    	+ '</h1></div>'
-    	+ '<div class="review_post_time"><h4>'
-    	+ dateString
-    	+ '</h4></div>'
-    	+ '<div class="review_text"><p>'
-    	+ reviewText
-    	+ '</p></div>';
+//Create New Review On Button Click
+function sendNews() {
+	var newNewsDiv = $.fn.createNewsNewDiv();
+	var newsId = $.fn.createNewsId();
+	$("#news_container").append(newNewsDiv);
+    localStorage.setItem(idNewsMask+newsId, newNewsDiv);
+	$.fn.clearNewsInputs();
+	alert('Новина успішно додана!');
+}
 
-		if(userName !== null && userName !== '' && reviewText !==null && reviewText !== '') {
-		   	$("#reviews").append(newDiv);
+//Create New Review Function
+$.fn.createNewsNewDiv = function() {
 
-			document.getElementById("send_user_name").value = "";
- 			document.getElementById("send_review").value = "";
-		} 
-		else {
-		   	alert("Вам необхідно заповнити поле з ім'ям та прізвищем, а також опис");
-		}
-
-
-
-    	
-	});
-});
-
-$(function() {
-    $('#send_news_button').click(function(){
-
-    	var uploadedImage = document.getElementById('my-file-selector').value;
+        var uploadedImage = document.getElementById('my-file-selector').value;
     	var newsContent = document.getElementById('news_text_admin_page').value;
-		var newsShortText = document.getElementById('news_short_text_admin_page').value;
 		var newsTitle = document.getElementById('news_title_admin_page').value;
 
-		if(newsContent !== null && newsContent !== '' && newsShortText !==null && newsShortText !== '' && newsTitle !==null && newsTitle !== '' && uploadedImage !==null && uploadedImage !== '') {
-			alert("Новина успішно додана!");
+		if(newsContent !== null && newsContent !== '' && newsTitle !==null && newsTitle !== '' && uploadedImage !==null && uploadedImage !== '') {
+	    	
+	    	var newsId = $.fn.createNewsId();
 
-			document.getElementById("news_text_admin_page").value = "";
- 			document.getElementById("news_short_text_admin_page").value = "";
- 			document.getElementById("news_title_admin_page").value = "";
- 			$("#remove_image_button").click()
-		} 
-		else {
-		   	alert("Вам необхідно заповнити усі поля");
+	    	var newNewsDiv = '<div class="col-md-4 news_table centered" data-newsdivid="'
+	    	+ idNewsMask+newsId
+	    	+ '"><a href="#" class="thumbnail"><img src="../img/news_image_2.jpg" alt="Зображення новини"><div class="caption"><p>' 
+	    	+ newsTitle 
+	    	+ '</p></div></a></div>'
+
+	    	return newNewsDiv;
+		} else {
+		   	$.fn.errorAlert();
 		}
-    	
-	});
-});
+}
+
+//Search All Existing Id And Create +1 Id Value
+$.fn.createNewsId = function() {
+	var newsId = 0;
+	var lsLen = localStorage.length;
+	if (lsLen > newsId) {
+		newsId = lsLen+1;
+		return newsId;
+	}
+	return newsId;
+}
+
+//Show All Existing
+$.fn.showNews = function() {
+	var lsLen = localStorage.length;
+	if (lsLen > 0) {
+		for (var i = 0; i < lsLen; i++){
+			var key = localStorage.key(i);
+			if (key.indexOf(idNewsMask) == 0) {
+				$("#news_container").append(localStorage.getItem(key));
+			}
+		}
+	}
+}
+
+$.fn.clearNewsInputs = function() {
+    document.getElementById("news_text_admin_page").value = "";
+	document.getElementById("news_title_admin_page").value = "";
+	$("#remove_image_button").click()
+}
+
+$.fn.errorAlert = function() {
+    alert("Вам необхідно заповнити усі поля");
+}
+
 
 
 		
